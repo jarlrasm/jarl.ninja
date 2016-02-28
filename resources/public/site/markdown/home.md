400b1b203df547f967b5b2f955d6459564dc7792
Jarl Sveinung Flø Rasmussen <jarl@jarlandying.com>
2016-02-28 22:37:44 +0100

    Rewrite for md goodness

---
+++ resources/public/site/markdown/home.md
@@ -0,0 +1,1 @@
+<img src="site/images/me.jpg" class="img-responsive .img-rounded">
--- src/jarl/ninja/core.cljs
+++ src/jarl/ninja/core.cljs
@@ -8,68 +8,78 @@
           [om.dom :as dom]
           [secretary.core :as secretary :refer-macros [defroute]]
           [goog.events :as events]
+          [goog.events :as events]
+          [goog.string ]
           [goog.history.EventType :as EventType]
           [markdown.core :refer [md->html]]
           [cljs-http.client :as http]
           [cljs.core.async :as async :refer [<!]])
    (:import goog.History))
+(def format goog.string.format)
+(enable-console-print!)
+(defonce app-state (atom {:current "" :document "" :site []}))
 
-(defonce app-state (atom {:current "root" :about "" :links ""}))
+(defn nav-item [page]
+  (n/nav-item {:key (:resource page) :href (format "#/%s" (:resource page))} (:name page)))
 
-(go
-    (let [response (<! (http/get "markdown/about.md"))]
-        (swap! app-state assoc :about (md->html(:body response)))
-      )
- )
-(go
-    (let [response (<! (http/get "markdown/links.md"))]
-        (swap! app-state assoc :links (md->html(:body response)))
-      )
- )
-(defn main-menu [active] (n/navbar
+(defn main-menu [active state]
+  (n/navbar
  {:brand (d/a {:href "#/"}
-              "Jarl")}
- (n/nav
+              (:name (first (filter #( = "" (:resource %)) (:pages (:site state))))))}
+ (apply n/nav
   {:collapsible? true
    :active-key active}
-  (n/nav-item {:key "about" :href "#/about"} "About me")
-  (n/nav-item {:key "links" :href "#/links"} "Links"))
+    (map nav-item (filter #(not (= "" (:resource %))) (:pages (:site state))))
+    )
   )
 )
-
-(defn links[state] (om.dom/div #js {:dangerouslySetInnerHTML #js {:__html (:links state)}} nil))
-(defn root[state] (d/img {:src "images/me.jpg" :class "img-responsive .img-rounded"}))
-(defn about[state] (om.dom/div #js {:dangerouslySetInnerHTML #js {:__html (:about state)}} nil))
 
 (defn main [state owner]
   (reify
     om/IRender
     (render [this]
      (dom/div {}
-      (main-menu  (:current state))
+      (main-menu  (:current state) state)
       (p/panel {}
-      (case (:current state)
-        "root" (root state)
-        "about" (about state)
-        "links" (links state)))))))
+      (om.dom/div #js {:dangerouslySetInnerHTML #js {:__html (:document state)}} nil)
+        ))))
+)
 
+(def load "<div>loading..<div>")
 
 (defn load-om [component page]
   (swap! app-state assoc :current page)
+  (swap! app-state assoc :document load)
+  (go
+    (let [allpages (:pages (:site (deref app-state)))]
+        (let [response (<! (http/get (str "site/markdown/" (:markdown(first (filter #(= page (:resource %)) allpages))))))]
+            (swap! app-state assoc :document (md->html(:body response)))
+      )
+        )
+   )
   (om/root component app-state
            {:target (. js/document (getElementById "app"))}))
 
 
-(defroute "/" []
-  (load-om main "root"))
+(defn addpage [page]
 
-(defroute "/about" []
-  (load-om main "about"))
+  (defroute (str "/" (:resource page))[]
+    (load-om main (:resource page)))
 
-(defroute "/links" []
-  (load-om main  "links"))
+  )
+(defn load-routes [pages]
+  (doseq [page (:pages pages)]
+    (addpage page)
+  )
+  )
+(go
+    (let [response (<! (http/get "site/site.json"))]
+        (swap! app-state assoc :site (:body response))
+        (load-routes (:body response))
+        (secretary/dispatch! "/")
+      )
+ )
 
-(secretary/dispatch! "/")
 
 (let [h (History.)]
   (goog.events/listen h EventType/NAVIGATE #(secretary/dispatch! (.-token %)))
